e10632692c56fd72c5ceb71e46a841c3cc4a97ba
nytopop <ericizoita@gmail.com>
2017-07-16 22:25:23 -0700

    Module headers for Tracker and Proto

---
+++ src/Network/Silver/Tracker.hs
@@ -0,0 +1,15 @@
+{- |
+Module      :  Network.Silver.Tracker
+Description :  Communication with tracker servers.
+Copyright   :  (c) Eric Izoita 2017
+License     :  BSD3
+
+Maintainer  :  ericizoita@gmail.com
+Stability   :  experimental
+Portability :  portable
+
+This module handles communications with bittorrent trackers.
+-}
+module Network.Silver.Tracker
+  (
+  ) where
