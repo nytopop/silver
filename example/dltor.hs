3172fc0b32c4c38087a7e2f35fa7186de761765e
Eric Izoita <ericizoita@gmail.com>
2017-07-24 14:40:03 -0700

    dltor scaffold

---
+++ example/dltor.hs
@@ -0,0 +1,19 @@
+module Main where
+
+-- | dltor <meta-info file> [OPTIONS]
+--         -s, --seed     : seed after completion
+--         -g, --greedy   : enable greedy mode
+--         -p, --progress : show progress
+--         -v, --verbose  : enable verbose mode
+--         -o OUTPUT      : set output directory (default is '.')
+--         -h, --help     : show this message
+--
+-- read torrent file
+-- decode metainfo
+-- call mkTorrent
+-- call dlTorrent <|>
+--      dlsTorrent
+--
+main :: IO
+main = do
+  print 0
