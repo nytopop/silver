c09f55f47335acce288b16d0562a6eec1df6b602
Eric Izoita <ericizoita@gmail.com>
2017-07-25 22:56:37 -0700

    line length <65

--- example/dltor.hs
+++ example/dltor.hs
@@ -1,13 +1,18 @@
 module Main where
 
 -- | dltor <meta-info file> [OPTIONS]
---     OPTIONS
---     -s, --seed     : seed after completion
---     -g, --greedy   : enable greedy mode
---     -p, --progress : show progress
---     -v, --verbose  : enable verbose mode
---     -o OUTPUT      : set output directory (default is '.')
---     -h, --help     : show this message
+--    -s, --seed
+--     ; seed after completion
+--    -g, --greedy
+--     ; enable greedy mode
+--    -p, --progress
+--     ; show progress
+--    -v, --verbose  
+--     ; enable verbose mode
+--    -o OUTPUT, --output OUTPUT
+--     ; set output directory (default is '.')
+--    -h, --help
+--     ; show this message
 --
 -- read torrent file
 -- decode metainfo
