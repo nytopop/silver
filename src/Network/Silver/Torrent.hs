d0de6f5b19f57bcdda302fd7c27398c5db4c0620
nytopop <ericizoita@gmail.com>
2017-12-10 12:41:54 -0800

    Torrent: initial peer communication
    currently not working because utp network hole punching isn't ready

--- src/Network/Silver/Torrent.hs
+++ src/Network/Silver/Torrent.hs
@@ -13,8 +13,10 @@ module Network.Silver.Torrent
   ) where
 
 -- Binary Data
+import Data.Binary (decodeOrFail, encode)
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as BL
 
 -- Concurrency & Flow Control
 import Control.Concurrent
@@ -31,12 +33,20 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 
+-- Networking
+import Network.Socket
+       (Family(AF_INET), SockAddr(..), SocketType(Stream), connect,
+        socket)
+import Network.Socket.ByteString.Lazy (recv, send)
+
 -- Internal
 import Network.Silver.Blob (Blob, PieceData(..), bGetPiece)
 import Network.Silver.Client (Client(..))
-import Network.Silver.Meta (PieceList(..))
+import Network.Silver.Meta (PieceList(..), infoHash, pieceList)
 import Network.Silver.Peers (getTrackerPeers)
+import Network.Silver.Proto (Handshake(..))
 
+{-
 -- Which peer source to use.
 data PeerSource
   = Tracker
@@ -55,21 +65,31 @@ data Strategy
   = Stream -- goes sequentially
   | Optimal -- max speed
   | Greedy -- max speed, greedy
-
+-}
+-- SO.
+-- we need to keep track of a few things:
+-- L0: current _network_ status of each _SockAddr_
+--     this should be handled by the lower level network api
+-- L1: current _protocol_ status of each _Peer_
+--     store in: Map ByteString PeerStatus
+--
+-- Map ByteString Socket     : sockets by peer id
+-- Map ByteString (PeerStatus, PeerStatus) : status by peer id
+--
 -- | Run client.
 runClient :: Client -> IO ()
 runClient c = do
-  tracker <- forkIO $ trackPeers c
-  mmm <-
-    forkIO $
-    fix $ \loop -> do
-      peers <- atomically $ readTVar $ clientPeers c
-      print peers
-      threadDelay (1000 * 1000 * 2)
-      loop
-  -- scanBlob blob pieces
+  _ <- forkIO $ trackPeers c
+  pieces <- scanBlob (clientBlob c) $ pieceList meta
+  atomically $ writeTVar (clientAvail c) pieces
+  -- 1. start a thread that listens for connections
+  -- 2. establish connections to all known peers
+  -- peers <- atomically $ readTVar (clientPeers c)
   threadDelay (1000 * 1000 * 100)
   return ()
+  return ()
+  where
+    meta = clientMeta c
 
 -- | Continually refresh the set of peers in c by periodically
 -- making requests to a tracker.
@@ -77,15 +97,49 @@ trackPeers :: Client -> IO ()
 trackPeers c =
   fix $ \loop -> do
     (peers, interval) <- getTrackerPeers c
+    print "Receiving peers..."
+    print peers
+    _ <- mapM ((flip talkToPeer) c) $ S.toList peers
     atomically $ do
       current <- readTVar (clientPeers c)
+      -- forkIO
+      -- connect
+      -- insert to 'Map ByteString Socket'
+      -- receive message
+      -- decode message
+      -- send to 'Chan Message'
       let updated = S.union current peers
+      --    newpeers = S.difference current updated
       writeTVar (clientPeers c) updated
-    print peers
-    print interval
+    putStrLn ("Requesting more peers in " ++ (show interval) ++ " seconds")
     threadDelay (1000 * 1000 * (fromInteger interval))
-    loop -- can be conditional for exit
+    loop
 
+talkToPeer :: SockAddr -> Client -> IO (ThreadId)
+talkToPeer addr c =
+  forkIO $ do
+    let hs = Handshake (infoHash $ clientMeta c) (clientID c)
+        hsmsg = encode hs
+    sock <- socket AF_INET Stream 0
+    connect sock addr
+    -- welp, that makes sense
+    -- we have to use uTP to
+    -- hole punch the target network...
+    print "we've connected..."
+    -- send our handshake
+    sent <- send sock hsmsg
+    print ("sent " ++ show sent ++ " bytes")
+    resp <- recv sock 16384
+    case mDec resp of
+      Nothing -> return ()
+      Just h -> do
+        print h
+
+mDec :: BL.ByteString -> Maybe Handshake
+mDec d =
+  case decodeOrFail d of
+    Left _ -> Nothing
+    Right (_, _, h) -> Just h
 
 -- | Verify a piece.
 --
@@ -96,9 +150,10 @@ verifyP (PieceData piece) checksum =
       newsum = (BS.pack . show . sha1) piece
   in checksum == newsum
 
--- | Scan for pieces in blob.
+-- | Scan for pieces in blob. Performs checksum verification
+-- on all stored pieces, so it may take some time.
 --
--- Note : Data within blob that does not hash correctly 
+-- Note: Data within the blob that does not hash correctly 
 -- will be treated as non-available, and thus will be
 -- overwritten throughout a download.
 scanBlob :: Blob -> PieceList -> IO (Map Integer Bool)
@@ -107,5 +162,6 @@ scanBlob blob (PieceList checksums) =
       indices = [0 .. len - 1]
       getPieces = sequence $ map (bGetPiece blob) indices
       have xs = zipWith verifyP xs checksums
-  in do pieces <- getPieces
+  in do print blob
+        pieces <- getPieces
         return $ M.fromList $ zip indices (have pieces)
