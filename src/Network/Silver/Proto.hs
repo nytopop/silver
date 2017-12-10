fd2e50630e89359460d2038f21ac130ac996b0a4
nytopop <ericizoita@gmail.com>
2017-12-10 12:40:39 -0800

    Proto: binary protocol serialization / deserialization

--- src/Network/Silver/Proto.hs
+++ src/Network/Silver/Proto.hs
@@ -10,63 +10,127 @@ Portability :  portable
 
 This module handles the bittorrent peer protocol.
 -}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Network.Silver.Proto
-  ( Peer
+  ( PeerStatus(..)
+  , Handshake(..)
+  , Message(..)
   ) where
 
-import Data.ByteString.Char8 (ByteString)
+-- Control
+import Control.Applicative ((<$>), (<*>))
+
+-- Binary Data
+import Data.Binary (Binary(..))
+import Data.Binary.Get (Get, getByteString)
+import Data.Binary.Put (putByteString)
 import qualified Data.ByteString.Char8 as BS
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
-import Data.Set (Set)
-import Network.Socket (SockAddr, Socket)
+import Data.ByteString.Char8 (ByteString)
+import Data.Word (Word32, Word8)
 
-data PeerCloud =
-  PeerCloud ByteString -- peer id
-            ByteString -- info hash
-            (Set Peer) -- known peers
-  deriving (Show, Eq)
-
-data Peer =
-  Peer ByteString -- peer id
-       SockAddr -- location
-  deriving (Show, Eq, Ord)
-
+-- Things to add
+-- - network hole punching over uTP through
+--   an intermediary unrestricted STUN host
 data PeerStatus
   = Dead
   | Alive Bool -- choked
           Bool -- interested
-  deriving (Show, Eq)
+  deriving (Show)
 
-data PeerConn =
-  PeerConn Socket -- active socket
-           SockAddr -- addr
-  deriving (Show, Eq)
+data Handshake =
+  Handshake ByteString -- info hash
+            ByteString -- peer id
+  deriving (Show)
 
---peerInsert :: PeerCloud -> Peer -> PeerCloud
---peerDelete :: PeerCloud -> Peer -> PeerCloud
---cGetPiece :: PeerCloud -> Int -> IO ByteString
--- | Return value of a message.
-data MessageStatus
-  = MsgFail
-  | MsgSuccess
-  deriving (Show, Eq)
+instance Binary Handshake where
+  get = do
+    len <- get :: Get Word8
+    case len of
+      19 -> do
+        proto <- getByteString (fromIntegral len)
+        if proto == "Bittorrent protocol"
+          then do
+            _ <- get :: Get Word32
+            _ <- get :: Get Word32
+            info <- getByteString 20
+            peer <- getByteString 20
+            return $ Handshake info peer
+          else fail "invalid protocol string"
+      _ -> fail "invalid handshake length"
+  put (Handshake info peer)
+    | BS.length info /= 20 = fail "invalid info hash length"
+    | BS.length peer /= 20 = fail "invalid peer id length"
+    | otherwise = do
+      put (19 :: Word8)
+      putByteString "Bittorrent protocol"
+      put (0 :: Word32)
+      put (0 :: Word32)
+      putByteString info
+      putByteString peer
 
--- | Protocol message.
 data Message
-  = MsgChoke
+  = MsgKeepAlive
+  | MsgChoke
   | MsgUnChoke
   | MsgInterested
   | MsgNotInterested
-  | MsgHave Int -- piece index
-  | MsgBitfield
-  | MsgRequest Int -- piece index
-               Int -- begin byte offset
-               Int -- length
-  | MsgPiece Int -- piece index
-             Int -- begin byte offset
+  | MsgHave Word32 -- piece index
+  | MsgBitfield ByteString
+  | MsgRequest Word32 -- piece index
+               Word32 -- begin byte offset
+               Word32 -- length
+  | MsgPiece Word32 -- piece index
+             Word32 -- begin byte offset
              ByteString -- piece data
-  | MsgCancel Int -- piece index
-              Int -- begin byte offset
-              Int -- length
-  deriving (Show, Eq)
+  | MsgCancel Word32 -- piece index
+              Word32 -- begin byte offset
+              Word32 -- length
+  deriving (Show)
+
+instance Binary Message where
+  get = do
+    len <- get :: Get Word32
+    case len of
+      0 -> return MsgKeepAlive
+      _ -> do
+        msgId <- get :: Get Word8
+        case msgId of
+          0 -> return MsgChoke
+          1 -> return MsgUnChoke
+          2 -> return MsgInterested
+          3 -> return MsgNotInterested
+          4 -> MsgHave <$> (get :: Get Word32)
+          5 -> MsgBitfield <$> getByteString (fromIntegral len - 1)
+          6 -> MsgRequest <$> get <*> get <*> get
+          7 -> do
+            let block = getByteString (fromIntegral len - 9)
+            MsgPiece <$> get <*> get <*> block
+          8 -> MsgCancel <$> get <*> get <*> get
+          _ -> fail "invalid protocol message id"
+  put MsgKeepAlive = do
+    put (0 :: Word32)
+  put MsgChoke = do
+    put (1 :: Word32) >> put (0 :: Word8)
+  put MsgUnChoke = do
+    put (1 :: Word32) >> put (1 :: Word8)
+  put MsgInterested = do
+    put (1 :: Word32) >> put (2 :: Word8)
+  put MsgNotInterested = do
+    put (1 :: Word32) >> put (3 :: Word8)
+  put (MsgHave idx) = do
+    put (5 :: Word32) >> put (4 :: Word8) >> put idx
+  put (MsgBitfield field) = do
+    let len = fromIntegral $ 1 + BS.length field
+    put (len :: Word32) >> put (5 :: Word8)
+    putByteString field
+  put (MsgRequest index offset len) = do
+    put (13 :: Word32) >> put (6 :: Word8)
+    put index >> put offset >> put len
+  put (MsgPiece index offset block) = do
+    let len = fromIntegral $ 9 + BS.length block
+    put (len :: Word32) >> put (7 :: Word8)
+    put index >> put offset >> put block
+  put (MsgCancel index offset len) = do
+    put (13 :: Word32) >> put (8 :: Word8)
+    put index >> put offset >> put len
